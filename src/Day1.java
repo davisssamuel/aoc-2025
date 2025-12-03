04f25f5030a6ad14f2ce5f945e5393dd23ac9304
davisssamuel <davissamuelg1@gmail.com>
2025-12-03 00:11:21 -0500

    feat: completed Day 1 Part 1 and Day 2 Part 2

---
+++ src/Day1.java
@@ -0,0 +1,31 @@
+int DIAL_START = 50;
+
+void main(String[] args) throws Exception {
+    if (args.length < 1) {
+        IO.println("Not enough arguments");
+        return;
+    }
+    String input = Files.readString(Paths.get(args[0]));
+
+    int pos = DIAL_START;
+    int zeroCount = 0;
+
+    for (String line : input.split("\n")) {
+        int clicks = Integer.parseInt(line.substring(1));
+        int delta = (line.charAt(0) == 'L') ? -clicks : clicks;
+
+        int old = pos;
+        pos = (pos + delta) % 100;
+        if (pos == 0) zeroCount++;
+
+        // Part 2 - I have no idea what I'm doing wrong at this point
+        double crosses = Math.abs(delta) / 100;
+        if (crosses < 1 && ((old > 0 && pos < 0) || (old < 0 && pos > 0))) {
+            zeroCount++;
+        } else {
+            zeroCount += Math.floor(crosses);
+        }
+    }
+
+    IO.println(zeroCount);
+}
