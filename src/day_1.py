85e3b8b382b107303aaf8296afe1c1f5eefe7810
davisssamuel <davissamuelg1@gmail.com>
2025-12-04 15:40:56 -0500

    feat: removed if name main block in Day 1 python solution

--- src/day_1.py
+++ src/day_1.py
@@ -1,27 +1,26 @@
-import sys
+from sys import argv
 
-if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("Not enough arguments")
-        exit()
+if len(argv) < 2:
+    print("Not enough arguments")
+    exit()
 
-    pos = 50
-    part_one_count = 0
-    part_two_count = 0
+pos = 50
+part_one_count = 0
+part_two_count = 0
 
-    with open(sys.argv[1]) as f:
-        for line in f:
-            clicks = int(line[1:])
-            delta = -clicks if line[0] == "L" else clicks
+with open(argv[1]) as f:
+    for line in f:
+        clicks = int(line[1:])
+        delta = -clicks if line[0] == "L" else clicks
 
-            start = pos
-            pos = (pos + delta) % 100
+        start = pos
+        pos = (pos + delta) % 100
 
-            if pos == 0:
-                part_one_count += 1
-            elif (delta > 0 and pos < start) or (start != 0 and delta < 0 and pos > start):
-                part_two_count += 1
+        if pos == 0:
+            part_one_count += 1
+        elif (delta > 0 and pos < start) or (start != 0 and delta < 0 and pos > start):
+            part_two_count += 1
 
-            part_two_count += clicks // 100
+        part_two_count += clicks // 100
 
-    print(f"Part 1: {part_one_count}\nPart 2: {part_one_count + part_two_count}")
+print(f"Part 1: {part_one_count}\nPart 2: {part_one_count + part_two_count}")
