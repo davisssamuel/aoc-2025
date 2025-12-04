e7454fadf6cc5340bf2a27d3926143878406e047
davisssamuel <davissamuelg1@gmail.com>
2025-12-04 15:30:57 -0500

    feat: updated Day 1 to print both parts 1 and 2

--- src/Day1.java
+++ src/Day1.java
@@ -6,29 +6,24 @@ void main(String[] args) throws Exception {
     String input = Files.readString(Paths.get(args[0]));
 
     int pos = 50;
-    int zeroCount = 0;
+    int partOneCount = 0;
+    int partTwoCount = 0;
 
     for (String line : input.split("\n")) {
         int clicks = Integer.parseInt(line.substring(1));
         int delta = (line.charAt(0) == 'L') ? -clicks : clicks;
 
-        // Part 1
-        // pos = Math.floorMod(pos + delta, 100);
-        // if (pos == 0) zeroCount++;
-
-        // Part 2
         int start = pos;
         pos = Math.floorMod(pos + delta, 100);
 
         if (pos == 0) {
-            zeroCount++;
-        } else if (delta > 0 && pos < start) {
-            zeroCount++;
-        } else if (start != 0 && delta < 0 && pos > start) {
-            zeroCount++;
+            partOneCount++;
+        } else if ((delta > 0 && pos < start) ||  (start != 0 && delta < 0 && pos > start)) {
+            partTwoCount++;
         }
-        zeroCount += Math.floorDiv(clicks, 100);
+
+        partTwoCount += Math.floorDiv(clicks, 100);
     }
 
-    IO.println(zeroCount);
+    IO.println("Part 1: " + partOneCount + "\nPart 2: " + (partOneCount + partTwoCount));
 }
--- src/day_1.py
+++ src/day_1.py
@@ -6,29 +6,22 @@ if __name__ == "__main__":
         exit()
 
     pos = 50
-    zero_count = 0
+    part_one_count = 0
+    part_two_count = 0
 
     with open(sys.argv[1]) as f:
         for line in f:
             clicks = int(line[1:])
             delta = -clicks if line[0] == "L" else clicks
 
-            # Part 1
-            # pos = (pos + delta) % 100
-            # if pos == 0:
-            #     zero_count += 1
-
-            # Part 2
             start = pos
             pos = (pos + delta) % 100
 
             if pos == 0:
-                zero_count += 1
-            elif delta > 0 and pos < start:
-                zero_count += 1
-            elif start != 0 and delta < 0 and pos > start:
-                zero_count += 1
+                part_one_count += 1
+            elif (delta > 0 and pos < start) or (start != 0 and delta < 0 and pos > start):
+                part_two_count += 1
 
-            zero_count += clicks // 100
+            part_two_count += clicks // 100
 
-    print(zero_count)
+    print(f"Part 1: {part_one_count}\nPart 2: {part_one_count + part_two_count}")
